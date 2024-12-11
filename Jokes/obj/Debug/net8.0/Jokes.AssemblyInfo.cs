c6bbbff10b2fcbba13705a4174a62036bbfada6a
giaom <aishamaabreh@outlook.com>
2024-12-11 10:31:27 -0800

    1- Changed UI in Jokes page, bolded some words, added lines and more space between jokes. File: Jokes>Views>Jokes>Index.cshtml.   2- Added Assets folder. Added 2 images under folder, for testing background purposes. FIle:  Jokes>Assets.

--- Jokes/obj/Debug/net8.0/Jokes.AssemblyInfo.cs
+++ Jokes/obj/Debug/net8.0/Jokes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jokes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa62e892cd7ec6a9a69f60a8566eca85200545d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9dc1ecad337d81143b668992d05ae5d677d24e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jokes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jokes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
