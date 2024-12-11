e7af94992c1c1d7dcb9d256f70ad11916719a1de
rohannpatell <64624780+Rohannpatel04@users.noreply.github.com>
2024-12-10 18:42:07 -0800

    Resolved Merge conflicts Part 2

--- Jokes/obj/Debug/net8.0/Jokes.AssemblyInfo.cs
+++ Jokes/obj/Debug/net8.0/Jokes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jokes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64de7410128651cab8c1c33bbb078832a544363e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa62e892cd7ec6a9a69f60a8566eca85200545d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jokes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jokes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
